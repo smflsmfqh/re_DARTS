c6c7ed7708c1ed50f4312a2f5bdb8aaa0c2ed26f
haneullee <skyee46@gmail.com>
2024-12-01 20:02:41 +0900

    re

--- evaluation:model.py
+++ evaluation:model.py
@@ -5,7 +5,7 @@ from src.operations import *
 from src.utils import drop_path
 
 
-class Cell(nn.Module):
+class Cell(nn.Module): #수정
 
   def __init__(self, genotype, C_prev_prev, C_prev, C, reduction, reduction_prev):
     super(Cell, self).__init__()
@@ -17,28 +17,36 @@ class Cell(nn.Module):
       self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0)
     self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0)
 
+    # search에서 genotype return값에 노드와 weight값을 추가함에 따라 수정
     if reduction:
-      op_names, indices = zip(*genotype.reduce)
+      op_data = genotype.reduce
       concat = genotype.reduce_concat
     else:
-      op_names, indices = zip(*genotype.normal)
+      op_data = genotype.normal
       concat = genotype.normal_concat
-    self._compile(C, op_names, indices, concat, reduction)
+    self._compile(C, op_data, concat, reduction)
 
-  def _compile(self, C, op_names, indices, concat, reduction):
-    assert len(op_names) == len(indices)
-    self._steps = len(op_names) // 2
+  def _compile(self, C, op_data, concat, reduction): 
+    # search에서 genotype return값에 노드와 weight값을 추가함에 따라 수정
+    assert len(op_data) == 11
+    self._steps = len(op_data) // 2
     self._concat = concat
     self.multiplier = len(concat)
 
     self._ops = nn.ModuleList()
-    for name, index in zip(op_names, indices):
-      stride = 2 if reduction and index < 2 else 1
-      op = OPS[name](C, stride, True)
-      self._ops += [op]
-    self._indices = indices
+    self._indices = []
+    self._start_nodes = []
+    self._weight = []
 
-  def forward(self, s0, s1, drop_prob):
+    for start_node, op_name, index, weight in op_data:
+      stride = 2 if reduction and index < 2 else 1
+      op = OPS[op_name](C, stride, True)
+      self._ops.append(op)
+      self._indices.append(index)
+      self._start_nodes.append(start_node)
+      self._weight.append(weight)
+
+  def forward(self, s0, s1, drop_prob): #수정
     s0 = self.preprocess0(s0)
     s1 = self.preprocess1(s1)
 
@@ -47,9 +55,9 @@ class Cell(nn.Module):
       h1 = states[self._indices[2*i]]
       h2 = states[self._indices[2*i+1]]
       op1 = self._ops[2*i]
-      op2 = self._ops[2*i+1]
+      op2 = self._ops[2*i+1] #개수 늘리기
       h1 = op1(h1)
-      h2 = op2(h2)
+      h2 = op2(h2) #개수 늘리기 -> threshold를 적용한다든지 여튼
       if self.training and drop_prob > 0.:
         if not isinstance(op1, Identity):
           h1 = drop_path(h1, drop_prob)
